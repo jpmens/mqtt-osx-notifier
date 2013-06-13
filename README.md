d16e4352aef45d6b4ccf399c09ecc37570191eb9
Jan-Piet Mens <jpmens@gmail.com>
2013-06-13 19:29:31 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,42 @@
+# MQTT-OSX-NOTIFIER
+
+Subscribes to any number of MQTT topics and notifies to the OSX notification
+area (whatever that may be called officially ...)
+
+Configuration is expected in the `mqtt-osx-notifier.ini` file (a sample template is
+provided, which has to be renamed):
+
+```ini
+[notifier]
+application=MQTT
+
+[mqtt]
+broker=localhost
+port=1883
+
+[topics]
+topic1 = test/one
+topic2 = twitter/emergency
+topic3 = jp/#
+```
+
+You can specify any number of topics in the `[topics]` section; the key name
+is ignored.
+
+`mqtt-osx-notifier` connect to the specified MQTT broker and subscribes to the list
+of topics. For each message received on a topic, `mqtt-prowl` will notify
+your OSX.
+
+Publish a test message to your broker, specifying one of the topics `mqtt-osx-notifier` is
+subscribed to:
+
+```
+mosquitto_pub -t test/one -m 'Introducing mqtt-osx-notifier. :-)'
+```
+
+After a few seconds, you should see the result on the top-right of your OSX machine.
+
+## Requirements
+
+* [pync](https://github.com/setem/pync) which uses the binary `terminal-notifier`. Note:
+  upon first launch, `pync` will download and extract `https://github.com/downloads/alloy/terminal-notifier/terminal-notifier_1.4.2.zip` into a directory `vendor/`.
---
+++ mqtt-osx-notifier.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013, Jan-Piet Mens <jpmens()gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+import mosquitto
+import sys, time
+from ConfigParser import SafeConfigParser
+from pync import Notifier   # https://github.com/SeTem/pync
+
+def config(inifile):
+    cf = SafeConfigParser()
+    cf.read(inifile)
+
+    return cf
+
+def on_connect(mosq, userdata, rc):
+    print "Connected"
+    for topic in userdata['topics']:
+        print "Subscribing to " + topic
+        mqttc.subscribe(topic, 0)
+
+def on_message(mosq, userdata, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+    cf = userdata['cf']
+    application = cf.get('notifier', 'application')
+
+    s = str(msg.topic) + "\n" + str(msg.payload)
+
+    Notifier.notify(s,  title=application)
+
+def on_subscribe(mosq, userdata, mid, granted_qos):
+    pass
+
+def on_disconnect(mosq, userdata, rc):
+    print "OOOOPS! disconnect"
+    time.sleep(10)
+
+
+cf = config('mqtt-osx-notifier.ini')
+
+topics = []
+for key, topic in cf.items('topics'):
+    topics.append(topic)
+
+userdata = {
+    'cf' : cf,
+    'topics' : topics,
+}
+
+mqttc = mosquitto.Mosquitto(userdata=userdata)
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_disconnect = on_disconnect
+mqttc.on_subscribe = on_subscribe
+
+mqttc.will_set('clients/mqtt-osx-notifier', payload="Adios!", qos=0, retain=False)
+
+mqttc.connect(cf.get('mqtt', 'broker'), cf.get('mqtt', 'port'), 60)
+
+try:
+    mqttc.loop_forever()
+except KeyboardInterrupt:
+    sys.exit(0)
+
