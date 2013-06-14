b7115adcaec4f6c8d9a162fa1a0a83784234b906
Jan-Piet Mens <jpmens@gmail.com>
2013-06-14 16:06:08 +0200

    Ensure port is retrieved as int

--- mqtt-osx-notifier.py
+++ mqtt-osx-notifier.py
@@ -70,7 +70,7 @@ mqttc.on_subscribe = on_subscribe
 
 mqttc.will_set('clients/mqtt-osx-notifier', payload="Adios!", qos=0, retain=False)
 
-mqttc.connect(cf.get('mqtt', 'broker'), int(cf.get('mqtt', 'port')), 60)
+mqttc.connect(cf.get('mqtt', 'broker'), cf.getint('mqtt', 'port'), 60)
 
 try:
     mqttc.loop_forever()
